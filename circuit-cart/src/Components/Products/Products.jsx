fe1f2979a244607a4c86efd2f05e77a319a88d06
unknown <mdamir4298@gmail.com>
2024-10-08 21:55:22 +0530

    initial setup

---
+++ circuit-cart/src/Components/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from "@mui-material/core"
+
+
+const Products = () => {
+  return (
+    <main>
+    <Grid container justify="center" spacing={4}></Grid>
+      
+    </main>
+  )
+}
+
+export default Products
